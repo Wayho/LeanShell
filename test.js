1c6e25018f66290ed8b33eb5f922e80ae8fae0c4
Wayho <34697764+Wayho@users.noreply.github.com>
2018-07-20 17:18:00 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-var OAuth = require('wechat-oauth');
-var auth = new OAuth('wx76cc8ae402a1c613', 'd9ccfdbc8cb5a75956f97d854fd0a09b');
-var url = auth.getAuthorizeURL('http://sale.leanapp.cn/', 'hehe', 'snsapi_userinfo');
-console.log('auth.getAuthorizeURL():', url);
+
