5f99f4a1f7e4149082b9c63b53cc15512f710392
Wayho <34697764+Wayho@users.noreply.github.com>
2018-07-20 17:05:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ AV.init({
   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
 });
 
+var config = {
+    wechat:{
+        appID:process.env.wechat_appID,
+        appSecret:process.env.wechat_appsecret,
+        token:process.env.wechat_token
+    }
+};
+
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
 var app = require('./app');
 
-console.log('process.env:', process.env);
+console.log('wechat:', config.wechat);
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
