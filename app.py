84dddcb776d0cd46b18fa133eda01cba65daf293
Wayho <34697764+Wayho@users.noreply.github.com>
2018-10-23 12:53:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+import chatterbot
+print(chatterbot.__version__)
+
 app = Flask(__name__)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+#english_bot.set_trainer(ChatterBotCorpusTrainer)
+#english_bot.train("chatterbot.corpus.english")
 
 
 @app.route("/")
